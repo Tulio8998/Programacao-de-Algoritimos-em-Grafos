dff6327e7fcc63b1511810bced3c9e61ac5549b2
tulio <tulio89lopes@gmail.com>
2025-08-09 14:15:01 -0300

    feat: implementa completo,regular e subgrafo

--- src/info.py
+++ src/info.py
@@ -18,16 +18,26 @@ def complemento(grafo):
 
 # retorna True se o grafo eh completo:
 def completo(grafo):
-    print("Metodo completo(grafo) nao foi implementado ainda!\n")
-    return None
+    for i in range(grafo.ordem()):
+        for j in range(grafo.ordem()):
+            if not grafo.possuiAresta(i, j):
+                return False
+    return True
 
 # retorna True se o grafo eh regular:
 def regular(grafo):
-    print("Metodo regular(grafo) nao foi implementado ainda!\n")
-    return None
-
+    grau = grafo.grau(0)
+    for i in range(grafo.ordem()):
+        if grafo.grau(i) != grau:
+            return False
+    return True
 
 # retorna um subgrafo induzido pelo conjunto de vertices:
 def subgrafo(grafo, vertices):
-    print("Metodo subgrafo(grafo, vertices) nao foi implementado ainda!\n")
-    return None
+    sg = listaAdjacencias.ListaAdjacencias(len(vertices))
+
+    for i in range(len(vertices)):
+        for j in range(len(vertices)):
+            if i != j and grafo.possuiAresta(vertices[i], vertices[j]):
+                sg.addAresta(i, j)
+    return sg
